d779a2b1660d744da3ebc73eb9fd48dc49672f5e
Adam Moss <adam.moss@nottingham.ac.uk>
2022-08-22 23:29:30 +0100

    Update run_sgd.py

--- scripts/run_sgd.py
+++ scripts/run_sgd.py
@@ -190,6 +190,8 @@ def train_model():
         onp.save(os.path.join(dirname, 'metrics.npy'), test_stats)
         print(test_stats)
 
+        key, = jax.random.split(key, 1)
+
         net_state, test_predictions = onp.asarray(
             predict_fn(net_apply, params, net_state, test_set, key))
         test_stats = train_utils.evaluate_metrics(test_predictions, test_set[1],
