07cfbfb59b740382fd2bcf59808765137d5eca2b
Adam Moss <adam.moss@nottingham.ac.uk>
2022-09-10 01:36:30 +0100

    Update mirabest.py

--- data_scripts/mirabest.py
+++ data_scripts/mirabest.py
@@ -14,17 +14,22 @@ from scripts.run_vi import get_args as get_vi_args
 from bnn_hmc.utils import train_utils
 from bnn_hmc.utils import script_utils
 
+dataset_name = 'mirabest/confident'
+model_name = 'lenet'
+num_repeats = 5
+
 cmd_args = get_vi_args()
 train_utils.set_up_jax(cmd_args.tpu_ip, cmd_args.use_float64)
 script_utils.print_visible_devices()
 
-cmd_args.dataset_name = 'mirabest/confident'
-cmd_args.model_name = 'lenet'
+cmd_args.dataset_name = dataset_name
+cmd_args.model_name = model_name
 cmd_args.train_split = 'train[:80%]'
 cmd_args.test_split = 'train[80%:]'
 
+# VI
+
 cmd_args.weight_decay = 5
-cmd_args.dir = 'runs/vi/mirabestc/0/'
 cmd_args.init_step_size = 1e-4
 cmd_args.num_epochs = 200
 cmd_args.eval_freq = 5
@@ -35,4 +40,7 @@ cmd_args.optimizer = 'Adam'
 cmd_args.vi_sigma_init = 0.01
 cmd_args.vi_ensemble_size = 20
 
-train_vi_model(cmd_args)
+for i in range(num_repeats):
+    cmd_args.seed = i
+    cmd_args.dir = 'runs/vi/mirabestc/%s/' % i
+    train_vi_model(cmd_args)
