73e52c4260c533ddc4f38e45c9f94338f663a4f8
Adam Moss <adam.moss@nottingham.ac.uk>
2022-09-12 10:41:32 +0100

    Update run.py

--- data_scripts/run.py
+++ data_scripts/run.py
@@ -1,6 +1,5 @@
 import sys
 import os
-from git import Repo
 
 sys.path.append(os.getcwd())
 sys.path.append(os.path.join(os.getcwd(), 'bnn_hmc'))
@@ -32,14 +31,7 @@ config = [
 num_repeats = 3
 num_ensemble_repeats = 10
 
-try:
-    repo = Repo('.')
-except:
-    repo = Repo('bnn_hmc')
-
 for c in config:
-
-    repo.git.checkout('main')
 
     cmd_args = get_vi_args()
     train_utils.set_up_jax(cmd_args.tpu_ip, cmd_args.use_float64)
@@ -72,9 +64,9 @@ for c in config:
         cmd_args.eval_split = None
         cmd_args.seed = i
         cmd_args.dir = 'runs/vi/%s/%s/' % (c['dataset'], i)
-        #train_vi_model(cmd_args)
+        train_vi_model(cmd_args)
         cmd_args.eval_split = c['eval']
-        #train_vi_model(cmd_args)
+        train_vi_model(cmd_args)
 
     # SGD
 
@@ -103,51 +95,15 @@ for c in config:
             cmd_args.eval_split = None
             cmd_args.seed = i*num_ensemble_repeats + j
             cmd_args.dir = 'runs/sdg/%s/%s/%s/' % (c['dataset'], i, j)
-            #train_sgd_model(cmd_args)
+            train_sgd_model(cmd_args)
             cmd_args.eval_split = c['eval']
-            #train_sgd_model(cmd_args)
+            train_sgd_model(cmd_args)
         cmd_args.ensemble_root = 'runs/sdg/%s/%s/*/*' % (c['dataset'], i)
-        #train_sgd_model(cmd_args)
-
-    # MCD
-
-    print('Performing MCD')
-
-    repo.git.checkout('dropout_wip')
-    time.sleep(5)
-
-    cmd_args = get_sgd_args()
-
-    cmd_args.dataset_name = c['dataset']
-    cmd_args.model_name = c['model'] + '_dropout'
-    cmd_args.train_split = c['train']
-    cmd_args.test_split = c['test']
-
-    cmd_args.weight_decay = 10
-    cmd_args.init_step_size = 3e-7
-    cmd_args.batch_size = c['batch_size']
-    cmd_args.scaling = c['scaling']
-    cmd_args.subset_train_to = c['subset_train_to']
-    cmd_args.builder_kwargs = c['builder_kwargs']
-    cmd_args.num_epochs = 200
-    cmd_args.eval_freq = 5
-    cmd_args.save_freq = 5
-    cmd_args.dropout_rate = 0.1
-    cmd_args.optimizer = 'SGD'
-
-    for i in range(num_repeats):
-        cmd_args.eval_split = None
-        cmd_args.seed = i
-        cmd_args.dir = 'runs/mcd/%s/%s/' % (c['dataset'], i)
-        train_sgd_model(cmd_args)
-        cmd_args.eval_split = c['eval']
         train_sgd_model(cmd_args)
 
     # HMC
 
     print('Performing HMC')
-
-    repo.git.checkout('main')
 
     cmd_args = get_hmc_args()
 
