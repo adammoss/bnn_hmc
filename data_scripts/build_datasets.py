d6fede3813b7dcd3fd7ef23e680cb3b02fa05b86
Adam Moss <adam.moss@nottingham.ac.uk>
2022-09-15 12:54:13 +0100

    Config

---
+++ data_scripts/build_datasets.py
@@ -0,0 +1,22 @@
+import sys
+import os
+
+sys.path.append(os.getcwd())
+sys.path.append(os.path.join(os.getcwd(), 'bnn_hmc'))
+
+from data_scripts.data_config import config
+
+import tensorflow_datasets as tfds
+import astro_datasets
+
+
+for c in config:
+    ds, info = tfds.load(name=c['dataset'], split=c['train'], with_info=True, as_supervised=True,
+                         builder_kwargs=c['builder_kwargs'])
+    ds, info = tfds.load(name=c['dataset'], split=c['test'], with_info=True, as_supervised=True,
+                         builder_kwargs=c['builder_kwargs'])
+    ds, info = tfds.load(name=c['dataset'], split=c['eval'], with_info=True, as_supervised=True,
+                         builder_kwargs=c['builder_kwargs'])
+    for ood in c['ood']:
+        ds, info = tfds.load(name=ood['dataset'], split=ood['eval'], with_info=True, as_supervised=True,
+                             builder_kwargs=ood['builder_kwargs'])
--- data_scripts/run.py
+++ data_scripts/run.py
@@ -15,71 +15,8 @@ from scripts.run_vi import train_model as train_vi_model
 from scripts.run_vi import get_args as get_vi_args
 from bnn_hmc.utils import train_utils
 from bnn_hmc.utils import script_utils
+from data_scripts.data_config import config
 
-
-config = [
-    {
-        'dataset': 'mirabest/confident',
-        'train': 'train[:80%]',
-        'test': 'train[80%:]',
-        'eval': 'test',
-        'batch_size': 53,
-        'ensemble_epochs': 100,
-        'subset_train_to': None,
-        'scaling': None,
-        'builder_kwargs': None,
-        'ood': [{
-            'dataset': 'mirabest/uncertain',
-            'eval': 'test',
-            'builder_kwargs': None,
-        }]
-    },
-    {
-        'dataset': 'slc/space',
-        'train': 'train[:80%]',
-        'test': 'train[80%:]',
-        'eval': 'test',
-        'batch_size': 50,
-        'ensemble_epochs': 20,
-        'subset_train_to': None,
-        'scaling': None,
-        'builder_kwargs': None,
-        'ood': []
-    },
-    {
-        'dataset': 'mlsst/Y10',
-        'train': 'train',
-        'test': 'validation',
-        'eval': 'test',
-        'batch_size': 50,
-        'ensemble_epochs': 100,
-        'subset_train_to': 20000,
-        'scaling': 'asinh',
-        'builder_kwargs': None,
-        'ood': [{
-            'dataset': 'mlsst/Y1',
-            'eval': 'test',
-            'builder_kwargs': None,
-        }]
-    },
-    {
-        'dataset': 'cmd',
-        'train': 'train[:90%]',
-        'test': 'train[90%:95%]',
-        'eval': 'train[95%:]',
-        'batch_size': 50,
-        'ensemble_epochs': 100,
-        'subset_train_to': None,
-        'scaling': 'asinh',
-        'builder_kwargs': '{"simulation": "IllustrisTNG", "field": "Mtot", "parameters": ["omegam"]}',
-        'ood': [{
-            'dataset': 'cmd',
-            'eval': 'train[95%:]',
-            'builder_kwargs': '{"simulation": "SIMBA", "field": "Mtot", "parameters": ["omegam"]}',
-        }]
-    },
-
-]
 
 model = 'lenet'
 num_repeats = 3
