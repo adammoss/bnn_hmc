062bd4a1bdfeafed0f44e7f9ddef4756c0170f3f
Adam Moss <adam.moss@nottingham.ac.uk>
2022-11-14 23:46:52 +0000

    best model

--- scripts/run_sgd.py
+++ scripts/run_sgd.py
@@ -135,6 +135,7 @@ def train_model(args):
         best_nll = jnp.inf
         for iteration in range(start_iteration, args.num_epochs):
             save_model = False
+            best_model = False
 
             (params, net_state, opt_state, logprob_avg, key), iteration_time = (
                 sgd_train_epoch(params, net_state, opt_state, train_set, key))
@@ -158,9 +159,10 @@ def train_model(args):
             # Save checkpoint
             if args.patience is None and (iteration % args.save_freq == 0 or iteration == args.num_epochs - 1):
                 save_model = True
+                best_model = True
 
             if save_model:
-                checkpoint_name = checkpoint_utils.make_checkpoint_name(iteration)
+                checkpoint_name = checkpoint_utils.make_checkpoint_name(iteration, best=best_model)
                 checkpoint_path = os.path.join(dirname, checkpoint_name)
                 checkpoint_dict = checkpoint_utils.make_sgd_checkpoint_dict(
                     iteration, params, net_state, opt_state, key)
